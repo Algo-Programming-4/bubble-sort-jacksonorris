fb85e5c612760ed100bb510f4e203c5df8c9c9ed
jacksonorris <jack.norri@gmail.com>
2024-10-07 19:04:32 -0600

    Update bubble.py

--- sorted_test.py
+++ sorted_test.py
@@ -1,7 +1,21 @@
-import sorting
 import random
+arr = [random.randint(1,100) for x in range(random.randint(1,10))]
 
-nums = [random.randint(1,100) for x in range(random.randint(1,100))]
+def selection_sort(arr):
+   
+   for x in range(len(arr)-1):      
+      small = x
 
-def test_sorted():
-  assert sorting.bubble(nums) == sorted(nums)
+      for i in range(x + 1, len(arr)):  
+                
+         if arr[i] < arr[small]:
+            
+            small = i  
+
+      temp = arr[x]
+      arr[x] = arr[small]
+      arr[small] = temp
+
+print("Unsorted List: ", arr)
+selection_sort(arr)
+print("Sorted List: ", arr)
